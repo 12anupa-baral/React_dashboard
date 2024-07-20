dd82cae59fed0a1a0be43f558b4bab1a08c640ef
Anupa Baral <86550755+12anupa-baral@users.noreply.github.com>
2024-07-20 11:54:58 +0545

    not found added

--- src/pages/404/NotFound.jsx
+++ src/pages/404/NotFound.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className='--center-all' style={{minHeight:"80vh"}}>
+     <h2>Not Found</h2> 
+     <p>Looks like the page you are looking for could not be found</p>
+     <Link to="/" className="--btn --btn-primary" style={{margin:"20px"}} >Back To Home</Link>
+      </div>
+     
   )
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import './Home.scss';
+
 
 function Home() {
+  const name ="Anupa Baral"
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className='container'> 
+      <h2>Home Page</h2>
+      <p>
+        Welcome to the <b>Home</b>Page
+      </p>
+      <p>
+        This page has <b>Redux Toolkit</b> set up.
+      </p>
+      <p> 
+        The name: <b>{name}</b> is stored in the dataSlice
+      </p>
+      </div>
+    </div>
   )
 }
 
